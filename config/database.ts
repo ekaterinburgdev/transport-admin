3e6724f3c8658bb53a2e8c21e4eb2851cdd6fbb4
agn <agn@skbkontur.ru>
2022-11-29 12:30:55 +0500

    Change config

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'postgres'),
+      host: env('DATABASE_HOST', 'postgres_tc'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('POSTGRES_USER', 'strapi'),
-      password: env('POSTGRES_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', 'strapi_tc'),
+      user: env('POSTGRES_USER', 'strapi_tc'),
+      password: env('POSTGRES_PASSWORD', 'strapi_tc'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },

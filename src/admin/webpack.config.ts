cbfc97c27f0272268da792fef9a2d239f9d65ef9
Design code <astakhova.ksyusha.07@mail.ru>
2023-04-15 18:00:01 +0300

    Automatically synchronized with admin interface

--- src/admin/webpack.config.ts
+++ src/admin/webpack.config.ts
@@ -4,6 +4,14 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = (config) => {
   config.plugins.push(new MonacoWebpackPlugin());
+  config.module = {
+    rules: [{
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false
+      }
+    }]
+  }
 
   return config;
 };

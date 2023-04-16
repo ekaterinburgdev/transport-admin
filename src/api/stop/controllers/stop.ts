6f7285ebfd1017807e1e07ab74d56e4d5f273956
ekbdev <astakhova.ksyusha.07@mail.ru>
2023-04-16 18:00:01 +0300

    Automatically synchronized with admin interface

---
+++ src/api/stop/controllers/stop.ts
@@ -0,0 +1,7 @@
+/**
+ *  stop controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::stop.stop');
---
+++ src/api/stop/routes/stop.ts
@@ -0,0 +1,7 @@
+/**
+ * stop router.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::stop.stop');
